78ef75fa0e8063f30fa49389be1b1486dded197c
Michael Dempsey <michael.dempsey@snorkel.ai>
2024-01-12 02:00:37 +0000

    Revert "[Core] Automatically set _node_ip_address for a driver when --node-ip-address is given (#37644) (#39316)"
    This reverts commit 3d6b86f014d0e3f57dc24fc95352b2882f09e6be.

--- python/ray/_private/worker.py
+++ python/ray/_private/worker.py
@@ -1367,7 +1367,9 @@ def init(
     )
     _redis_max_memory: Optional[int] = kwargs.pop("_redis_max_memory", None)
     _plasma_directory: Optional[str] = kwargs.pop("_plasma_directory", None)
-    _node_ip_address: str = kwargs.pop("_node_ip_address", None)
+    _node_ip_address: str = kwargs.pop(
+        "_node_ip_address", ray_constants.NODE_DEFAULT_IP
+    )
     _driver_object_store_memory: Optional[int] = kwargs.pop(
         "_driver_object_store_memory", None
     )
@@ -1539,6 +1541,10 @@ def init(
         gcs_address = bootstrap_address
         logger.info("Connecting to existing Ray cluster at address: %s...", gcs_address)
 
+    if _node_ip_address is not None:
+        node_ip_address = services.resolve_ip_for_localhost(_node_ip_address)
+    raylet_ip_address = node_ip_address
+
     if local_mode:
         driver_mode = LOCAL_MODE
         warnings.warn(
@@ -1583,7 +1589,8 @@ def init(
 
         # Use a random port by not specifying Redis port / GCS server port.
         ray_params = ray._private.parameter.RayParams(
-            node_ip_address=_node_ip_address,
+            node_ip_address=node_ip_address,
+            raylet_ip_address=raylet_ip_address,
             object_ref_seed=None,
             driver_mode=driver_mode,
             redirect_output=None,
@@ -1666,7 +1673,8 @@ def init(
 
         # In this case, we only need to connect the node.
         ray_params = ray._private.parameter.RayParams(
-            node_ip_address=_node_ip_address,
+            node_ip_address=node_ip_address,
+            raylet_ip_address=raylet_ip_address,
             gcs_address=gcs_address,
             redis_address=redis_address,
             redis_password=_redis_password,
