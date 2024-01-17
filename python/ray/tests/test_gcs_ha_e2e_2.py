dd3e090bab11f5f973c529ac9d1d6167206f76eb
Michael Dempsey <michael.dempsey@snorkel.ai>
2024-01-17 00:35:15 +0000

    Revert "[Core] Tmp_dir is incorrectly set from the driver when `ray start --temp-dir` is given (#39443) (#39518)"
    This reverts commit 0cf879375ab0f6773e2e77f888dc7a6f0b9d9228.

--- python/ray/tests/test_gcs_ha_e2e_2.py
+++ python/ray/tests/test_gcs_ha_e2e_2.py
@@ -7,6 +7,7 @@ from ray.tests.conftest_docker import *  # noqa
 
 # TODO(sang): Also check temp dir
 @pytest.mark.skipif(sys.platform != "linux", reason="Only works on linux.")
+@pytest.mark.skip(reason="Currently flaky.")
 def test_ray_session_name_preserved(docker_cluster):
     get_nodes_script = """
 import ray
