8b051e7b8ebab81a43f6de8b269ee7606632567b
Hiromu Hota <hiromu.hota@gmail.com>
2025-05-09 20:50:15 +0000

    Fix test_ray_dask_resources

--- python/ray/util/dask/scheduler.py
+++ python/ray/util/dask/scheduler.py
@@ -155,8 +155,6 @@ def ray_dask_get(dsk, keys, **kwargs):
     # Take out the dask graph if it is an Expr for dask>=2025.4.0.
     if not isinstance(dsk, Mapping):
         dsk = dsk.__dask_graph__()
-    # Make sure the graph is in the new format
-    dsk = convert_legacy_graph(dsk)
 
     scoped_ray_remote_args = _build_key_scoped_ray_remote_args(
         dsk, annotations, ray_remote_args
@@ -172,7 +170,8 @@ def ray_dask_get(dsk, keys, **kwargs):
             ray_postsubmit_all_cbs,
             ray_finish_cbs,
         ) = unpack_ray_callbacks(ray_callbacks)
-
+        # Make sure the graph is in the new format
+        dsk = convert_legacy_graph(dsk)
         # NOTE: We hijack Dask's `get_async` function, injecting a different
         # task executor.
         object_refs = get_async(
